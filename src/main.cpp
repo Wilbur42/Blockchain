81e1a6666c3cb987a0579ada3cf0d98425ec2a06
Wilbur42 <Will007ward@gmail.com>
2023-05-31 20:19:25 +0100

    Added Block Validation

--- src/main.cpp
+++ src/main.cpp
@@ -39,11 +39,48 @@ public:
 
     std::string calculateHash(Block& block) {
         // Implement hash calculation
+        // Need to find a suitable library
+        // For now, just return a temporary value
         return "hash";
     }
 
     void addBlock(Block& block) {
         // Validate the block and add it to the chain
+        if (isValidBlock(block)) {
+            chain.push_back(block);
+        }
+        else {
+            std::cout << "Invalid block. Cannot add to the chain." << std::endl;
+        }
+    }
+
+    bool isValidBlock(Block& block) {
+        // Implement validation rules
+        const Block& lastBlock = getLastBlock();
+
+        // Check if the previous hash matches the hash of the last block
+        if (block.previousHash != lastBlock.hash) {
+            return false;
+        }
+
+        // Check block number is one more than the last block
+        if (block.blockNumber != lastBlock.blockNumber + 1) {
+            return false;
+        }
+
+        // Check if the timestamp is in the past
+        std::time_t currentTime = std::time(nullptr);
+        if (block.timestamp > currentTime) {
+            return false;
+        }
+
+        // Check for correct hash value
+        std::string calculatedHash = calculateHash(block);
+        if (block.hash != calculatedHash) {
+            return false;
+        }
+
+        return true;
     }
 
     const Block& getLastBlock() const {
@@ -71,6 +108,21 @@ int main() {
 
     // Output the genesis block
     Block genesisBlock = blockchain.getLastBlock();
-    std::cout << "Genesis block: " << genesisBlock.hash << std::endl;
+    std::cout << "Genesis block: " << genesisBlock.blockNumber << std::endl;
 
+    // Create a new block
+    Block newBlock;
+    newBlock.blockNumber = 1;
+    newBlock.previousHash = genesisBlock.hash;
+    newBlock.timestamp = std::time(nullptr);
+    newBlock.transactions.push_back("Transaction 1");
+    newBlock.transactions.push_back("Transaction 2");
+    newBlock.hash = blockchain.calculateHash(newBlock);
+
+    // Add the new block to the chain
+    blockchain.addBlock(newBlock);
+
+    // Output the new block
+    Block lastBlock = blockchain.getLastBlock();
+    std::cout << "New block: " << lastBlock.blockNumber << std::endl;
 }
