e8bbcbd6ba64bf910f54f460af4fa3462b166674
Wilbur42 <Will007ward@gmail.com>
2023-06-01 15:34:36 +0100

    Added Validators

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 #include <ctime>
+#include <random>
 
 #include <hash.hpp>
 
@@ -99,14 +100,74 @@ public:
 
     void addValidator(Validator& validator) {
         // Add a new validator to the network
+        if (isValidValidator(validator)) {
+            validators.push_back(validator);
+        }
     }
 
-    void selectValidator() {
+    bool isValidValidator(const Validator& validator) {
+        // Check if the validator is valid
+
+        // Check if the validator is already in the network
+        for (const Validator& v : validators) {
+            if (v.publicKey == validator.publicKey) {
+                return false;
+            }
+        }
+
+        // Check if the validator has enough stake
+        if (validator.stake <= 0) {
+            return false;
+        }
+
+        return true;
+    }
+
+    const Validator& selectValidator() const {
         // Randomly select a validator based on their stake
+
+        // Calculate the total stake
+        int totalStake = 0;
+        for (const Validator& validator : validators) {
+            totalStake += validator.stake;
+        }
+
+        // Generate a random number between 0 and totalStake
+        std::random_device rd;
+        int randomNumber = rd() % totalStake + 1;
+
+        std::cout << "Random number: " << randomNumber << std::endl;
+
+        // Select the validator based on the random number
+        int stakeSum = 0;
+        for (const Validator& validator : validators) {
+            stakeSum += validator.stake;
+            if (stakeSum >= randomNumber) {
+                // Validator selected
+                return validator;
+            }
+        }
+
+        // Throw an error if no validator is selected
+        throw std::runtime_error("No validator selected");
     }
 
     void validateBlock(Block& block) {
         // Validate the block using PoS consensus rules
+
+        // Select a validator
+        const Validator& validator = selectValidator();
+
+        // Check if the validator is valid
+        if (!isValidValidator(validator)) {
+            std::cout << "Invalid validator" << std::endl;
+            return;
+        }
+
+        // Additional checks and validation rules...
+
+        // Block is valid, add it to the chain
+        chain.push_back(block);
     }
 };
 
@@ -115,7 +176,7 @@ int main() {
     Blockchain blockchain;
 
     // Output the genesis block
-    Block genesisBlock = blockchain.getLastBlock();
+    const Block& genesisBlock = blockchain.getLastBlock();
     std::cout << "Genesis block: " << genesisBlock.hash << std::endl;
 
     // Create a new block
@@ -131,6 +192,30 @@ int main() {
     blockchain.addBlock(newBlock);
 
     // Output the new block
-    Block lastBlock = blockchain.getLastBlock();
-    std::cout << "block " << lastBlock.blockNumber << ": " << lastBlock.hash << std::endl;
+    const Block& lastBlock = blockchain.getLastBlock();
+    std::cout << "Block " << lastBlock.blockNumber << ": " << lastBlock.hash << std::endl;
+
+    // Create a new validator
+    Validator validator;
+    validator.publicKey = "publicKey";
+    validator.stake = 100;
+
+    // Add the validator to the network
+    blockchain.addValidator(validator);
+
+    // Create a second validator
+    Validator validator2;
+    validator2.publicKey = "publicKey2";
+    validator2.stake = 200;
+
+    // Add the second validator to the network
+    blockchain.addValidator(validator2);
+
+    // Select a validator
+    const Validator& selectedValidator = blockchain.selectValidator();
+
+    // Output the selected validator
+    std::cout << "Selected validator: " << selectedValidator.publicKey << std::endl;
+
+    return 0;
 }
